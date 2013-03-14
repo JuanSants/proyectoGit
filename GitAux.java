75e157415d29fe42bb214f3080df60814f434b07
Igna-Olivares <ignaolivares@gmail.com>
2013-03-14 16:13:20 +0100

    Modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -15,11 +15,14 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	System.out.println("modificado por alum 2");
     }
     
     public void metodoComunitario(){
 	//Comentario Incluido para pruebas
         System.out.println("Aqui escribimos todos");
+	// modificacaciones alumno 2
+	System.out.println("Alumno 2 inserta esta sentencia");
     }
 
     public void testGitAux(){
